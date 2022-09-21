12b8a7669fb45de2663c40569d9a037be7976a9d
Richmond Nkansah <richmondnkansah@gmail.com>
2022-09-21 13:53:55 -0400

    commit tree d4f863afc084bf7b48c2307047c37513d9811401

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+const newArray = [...cats, name];
+return newArray;
+}
+
+function prependCat(name){
+    const newArray2 = [name ,...cats];
+    return newArray2;
+}
+
+function removeLastCat(){
+    const newArray3 = cats.slice(0,-1);
+    return newArray3;
+
+}
+
+function  removeFirstCat(){
+    const newArray4 = cats.slice(1);
+    return newArray4;
+}
